65dd42de7e4b61a1c6db14af08bd2d40dd1bf739
OldMan35 <kostya_kv@mail.ru>
2021-08-14 09:31:01 +0300

    Initial commit

---
+++ src/main/java/com/example/purchaseanalysis/PurchaseAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.example.purchaseanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchaseAnalysisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PurchaseAnalysisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/purchaseanalysis/PurchaseAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.purchaseanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchaseAnalysisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
