ed5620d6a9fc4cd6df97f9c816c770a76dc98482
OldMan35 <kostya_kv@mail.ru>
2021-08-15 07:26:26 +0300

    add PurchaseParser.java

--- src/main/java/com/example/purchaseanalysis/controller/PurchaseController.java
+++ src/main/java/com/example/purchaseanalysis/controller/PurchaseController.java
@@ -1,14 +1,10 @@
 package com.example.purchaseanalysis.controller;
 
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class PurchaseController {
-
-    @Value("${upload.path}")
-    private String uploadPath;
 
 
 }
